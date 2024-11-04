b68749a020441938c1dba241481155b4bae4db44
Ethan Skamarock <ethan@skamarock.net>
2024-11-04 12:32:57 -0700

    updated create statements. 'user' is a postgres reserved word

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,16 +1,14 @@
-
-
-CREATE TABLE User (
-    UserId Serial PRIMARY KEY,
+CREATE TABLE Client (
+    ClientId Serial PRIMARY KEY,
     Username VARCHAR(45) NOT NULL,
     Password VARCHAR(300) NOT NULL,
     Email VARCHAR(45) NOT NULL,
     FirstName VARCHAR(45),
     LastName VARCHAR(45),
-    UserImage VARCHAR(45),
-    CONSTRAINT fk_user
-        FOREIGN KEY(FriendId)
-            REFERENCES User(UserId)
+    UserImage VARCHAR(45)
+    -- CONSTRAINT fk_user
+    --     FOREIGN KEY(FriendId)
+    --         REFERENCES User(UserId)
 );
 
 CREATE TABLE Movie (
@@ -25,20 +23,23 @@ CREATE TABLE Review (
     ReviewBody TEXT NOT NULL,
     MovieRating INT NOT NULL,
     ReviewRating INT,
-    CONSTRAINT fk_user
-        FOREIGN KEY(UserId)
-            REFERENCES User(UserId),
-    CONSTRAINT fk_movie
-        FOREIGN KEY(MovieId)
-            REFERENCES Movie(MovieId)
+    ClientId INT NOT NULL REFERENCES Client(ClientId),
+    -- CONSTRAINT fk_user
+    --     FOREIGN KEY(UserId)
+    --         REFERENCES User(UserId),
+    MovieId INT NOT NULL REFERENCES Movie(MovieId)
+    -- CONSTRAINT fk_movie
+    --     FOREIGN KEY(MovieId)
+    --         REFERENCES Movie(MovieId)
 );
 
 CREATE TABLE Comment (
     CommentId SERIAL PRIMARY KEY,
     CommentBody TEXT NOT NULL,
-    CONSTRAINT fk_review
-        FOREIGN KEY(ReviewId)
-            REFERENCES Review(ReviewId)
+    ReviewId INT NOT NULL REFERENCES Review(ReviewId)
+    -- CONSTRAINT fk_review
+    --    FOREIGN KEY(ReviewId)
+    --        REFERENCES Review(ReviewId)
 );
 
 
