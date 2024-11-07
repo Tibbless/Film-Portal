b2265fe651fc0289281d255aebbb71cd3a47c064
Ethan Skamarock <ethan@skamarock.net>
2024-11-07 15:41:26 -0700

    one user insert

---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO Client (Username, Password, Email, FirstName, LastName, UserImage)
+VALUES ('Eskam', 
+	'5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
+	'etsk5997@colorado.edu',
+	'Ethan',
+	'Skamarock',
+	'https://avatars.githubusercontent.com/u/48768892?s=400&u=af251595b83ce2640ea7a35c238d889bb3619e65&v=4');
