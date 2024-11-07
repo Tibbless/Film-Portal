5004b90b7df21e26971a8650c0417f11d7b47ed6
Nolan Tibbles <123203268+Tibbless@users.noreply.github.com>
2024-11-07 09:54:18 -0700

    Update index.js
    Added items from mains index.js to allow merge

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -56,9 +56,43 @@ db.connect()
     console.log('ERROR', error.message || error);
   });
 
-  app.get('/login', (req, res) => {
-    res.render('pages/login');
-  });
+const axios = require('axios');
+
+function getMovies(query) {
+    var moviesData = []
+
+    axios({
+        url: `https://api.themoviedb.org/3/search/movie`,
+        method: `GET`,
+        params: {
+            query: query,
+            include_adult: false,
+            language: "en-USA",
+            page: 1, // Limit API calls
+            api_key: process.env.API_KEY
+        }
+    }).then(res => res.data.results.forEach((result) => {
+        var image = null
+
+        if (result.poster_path) {
+            image = `https://image.tmdb.org/t/p/original/${result.poster_path}`
+        }
+
+        moviesData[moviesData.length] = {
+            title: result.title,
+            release_date: result.release_date,
+            description: result.overview,
+            image: image
+        }
+
+        return moviesData
+    }))
+      .catch(error => console.log(error))
+}
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
